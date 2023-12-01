3c2af8775ca89cf7dcf58dd13b944513b92d2eb5
yuvalgos <yuvalgos@gmail.com>
2023-12-01 12:17:38 +0200

    SAM segmentation works very dirty

--- main_open3d_render.py
+++ main_open3d_render.py
@@ -85,11 +85,11 @@ if __name__ == '__main__':
     cam_pos = cam_1_pos
     cam_frame_rotation = xy_axes_to_frame_rotation(cam_1_xy_axes[0], cam_1_xy_axes[1])
 
-    # cam_frame_rotation = np.eye(3)
+    cam_frame_rotation = np.eye(3)
     # cam_frame_rotation = np.array([[0.7071068, -0.7071068, 0.000], [0.7071068, 0.7071068, 0], [0, 0, 1]]).T # 45deg around Z works
     # cam_frame_rotation = np.array([[0.9396926, 0, 0.3420202], [0, 1, 0], [-0.3420202, 0, 0.9396926]]).T # 20deg around Y
     # cam_frame_rotation = np.array([[1,  0,  0], [0,  0.9396926, -0.3420202], [0,  0.3420202,  0.9396926]]) # 20deg around X
-    # cam_pos = np.array([-0.3,0.4,2])
+    cam_pos = np.array([-0.3,0.4,2])
 
     # extrinsic_matrix = extrinsic_matrix_from_rotation_translatin(cam_frame_rotation, cam_pos)
 
@@ -116,6 +116,9 @@ if __name__ == '__main__':
 
     plt.imshow(res)
     plt.show()
+
+    # save res:
+    plt.imsave('mug.png', res)
 
     # plt.plot(interest_points_im_plane[:, 0], interest_points_im_plane[:, 1], '.', color='red',
     #          markersize=1, alpha=0.5)
---
+++ segment.py
@@ -0,0 +1,73 @@
+from segment_anything import SamPredictor, sam_model_registry
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels == 1]
+    neg_points = coords[labels == 0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white',
+               linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white',
+               linewidth=1.25)
+
+
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
+
+
+im = cv2.imread("mug.png")
+im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+sam = sam_model_registry["vit_b"](checkpoint="models/sam_vit_b_01ec64.pth")
+predictor = SamPredictor(sam)
+predictor.set_image(im)
+
+plt.imshow(im)
+
+points_fg = None
+points_labels = None
+points_fg = np.array([[135,135]])
+points_labels = np.array([1])
+# show_points(points_fg, points_labels, plt.gca())
+# plt.show()
+
+box = np.array([[100, 75, 190, 160]])
+# show_box(box[0], plt.gca())
+
+# segment:
+masks, scores, logits = predictor.predict(
+    point_coords=points_fg,
+    point_labels=points_labels,
+    box=box,
+    multimask_output=True,
+)
+
+# plot image and masks:
+for i, (mask, score) in enumerate(zip(masks, scores)):
+    plt.figure(figsize=(10,10))
+    plt.imshow(im)
+    show_mask(mask, plt.gca())
+    show_points(points_fg, points_labels, plt.gca())
+    show_box(box[0], plt.gca())
+    plt.title(f"Mask {i+1}, Score: {score:.3f}", fontsize=18)
+    plt.axis('off')
+    plt.show()
+
+# plot all masks:
+# for mask in masks:
+#     m = mask['segmentation']
+#     plt.imshow(m)
+#     plt.show()
