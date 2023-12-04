after fixing R computation and some code organization

camera_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from scipy.spatial.transform import Rotation
 
 
 def homogeneous_to_cartesian(homogeneous):
@@ -38,15 +39,20 @@ def extrinsic_matrix_from_rotation_translatin(rotation_matrix, translation):
 
 
 def xy_axes_to_frame_rotation(x_axis, y_axis):
+    y_axis = np.array(y_axis)
+    x_axis = np.array(x_axis)
+    # reduce non orthogonal part of y and x:
+    y_axis = y_axis - y_axis.T @ x_axis * x_axis
+
     z_axis = np.cross(x_axis, y_axis)
 
     x_axis = x_axis / np.linalg.norm(x_axis)
     y_axis = y_axis / np.linalg.norm(y_axis)
     z_axis = z_axis / np.linalg.norm(z_axis)
 
-    assert np.dot(x_axis, y_axis) < 1e-4, "x and y axes are not orthogonal"
-    assert np.dot(x_axis, z_axis) < 1e-4
-    assert np.dot(y_axis, z_axis) < 1e-4
+    assert np.dot(x_axis, y_axis) < 1e-5, "x and y axes are not orthogonal"
+    assert np.dot(x_axis, z_axis) < 1e-5
+    assert np.dot(y_axis, z_axis) < 1e-5
 
     return np.array([x_axis, y_axis, z_axis]).T
 
@@ -57,8 +63,16 @@ def get_torch3d_R_T(cam_frame_rotation, cam_pos):
     muj2torch = np.array([[-1, 0, 0], [0, 1, 0], [0, 0, -1]])
 
     # we invert the rotation by transposing the matrix, then move from mujoco to torch3d coordinates
-    R = muj2torch @ cam_frame_rotation.T
+    R_temp = muj2torch @ cam_frame_rotation.T
+    T = - R_temp @ cam_pos
 
-    T = - R @ cam_pos
+    # torch 3d uses euler angles in the order ZYX, mujoco uses XYZ, in addition rotation around Y axis is opposite in
+    # pytorch 3d. For some reason in order to compute the T vector we need to use the non-inverted rotation and order R.
+    # Trust me, I had some bad time figuring this out.
+    r = Rotation.from_matrix(cam_frame_rotation)
+    euler = r.as_euler('xyz', degrees=True)
+    euler_new = np.array([euler[2], -euler[1], euler[0]])
+    cam_frame_rotation = Rotation.from_euler('zyx', euler_new, degrees=True).as_matrix()
+    R = muj2torch @ cam_frame_rotation.T
 
     return torch.tensor(R, dtype=torch.float32), torch.tensor(T, dtype=torch.float32)