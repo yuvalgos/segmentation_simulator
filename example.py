1ad93df1e7f5ab707ea735144da256a2bb0a4627
yuvalgos <yuvalgos@gmail.com>
2023-12-10 13:55:56 +0200

    removed sam heavy model weights from the repo

--- example.py
+++ example.py
@@ -1,8 +1,13 @@
 import numpy as np
 from camera_simulator import CameraSimulator
-from camera_utils import xy_axes_to_frame_rotation
+from camera_utils import xy_axes_to_frame_rotation, get_torch3d_R_T
+from matplotlib import pyplot as plt
 from sam_segmentation import SAMSegmentation
-from utils import plot_segmentation_mask
+from utils import plot_segmentation_mask, plot_grid_segmentation_masks
+from geometric_mesh_segmentation import CameraParameters, get_mesh_segmentation_batch
+
+mesh = "./data/meshes/mug.obj"
+scale = 0.02
 
 cam_pos = [0.5, -0.5, 1.75]
 cam_xy_axes = [[0.685, 0.728, 0.000], [-0.487, 0.458, 0.743]]
@@ -12,23 +17,58 @@ cam_fov = 45
 cam_znear, cam_zfar = 0.1, 100
 
 table_height = 1
-obj_position = [0, 0, 0.08 + table_height]
-obj_orientation = [2.1, 0, 1.57]  # TODO move to stable pose set file.
-obj_orientations = [
-    [2.1, 0, 1.57]
+obj_position_actual = [0, 0, 0.08 + table_height]
+obj_orientation_actual = [2.1, 0, 1.57]
+# TODO move to stable pose set file.
+obj_positions = [
+    [0, 0, 0.08 + table_height],
+    [0, 0, 0.08 + table_height],
+    [0, 0, 0.12 + table_height],
+    [0, 0, 0.12 + table_height],
 ]
-
+obj_orientations = [
+    [2.1, 0, 1.57],
+    [0, 0, 1.57],
+    [0, -0.2, 0],
+    [3.14, -0.2, 0]
+]
 
 if __name__ == "__main__":
     mask = np.zeros([cam_resy, cam_resx])
 
-    cam_sim = CameraSimulator(resolution=(cam_resy, cam_resx), fovy=cam_fov,)
-    cam_sim.set_manipulated_object_position(obj_position)
-    cam_sim.set_manipulated_object_orientation_euler(obj_orientation)
-    im = cam_sim.render(cam_frame_R, cam_pos)
+    cam_sim = CameraSimulator(resolution=(cam_resy, cam_resx), fovy=cam_fov, )
 
-    # sam = SAMSegmentation()
-    # mask, score = sam.segment_image_center(im, best_out_of_3=True)
+    # generate 4 images for four poses and plot them side by side:
+    images = []
+    fig, axs = plt.subplots(1, 4, figsize=(20, 5))
+    for obj_position, obj_orientation, ax in zip(obj_positions, obj_orientations, axs):
+        cam_sim.set_manipulated_object_position(obj_position)
+        cam_sim.set_manipulated_object_orientation_euler(obj_orientation)
+        im = cam_sim.render(cam_frame_R, cam_pos)
+        ax.imshow(im)
+        ax.axis('off')
+        images.append(im)
+    plt.show()
 
-    plot_segmentation_mask(im, mask)
+    # plot four geometric segmentations (predicted segmentation):
+    masks = []
+    for obj_position, obj_orientation in zip(obj_positions, obj_orientations):
+        # can't really batch here since pose is different
+        R, T = get_torch3d_R_T(cam_frame_R, cam_pos)
+        cam_params = CameraParameters(res_x=cam_resx, res_y=cam_resy, fov=cam_fov, R=R, T=T,
+                                      z_near=cam_znear, z_far=cam_zfar)
+        mask = get_mesh_segmentation_batch(mesh_path=mesh, scale=scale, cameras_parameters=cam_params,
+                                           position=obj_position, orientation=obj_orientation, device='cpu')
+        masks.append(mask)
+    plot_grid_segmentation_masks(images, masks, (1, 4), mask_alpha=0.95)
 
+    # use sam to segment actual image and show it
+    cam_sim.set_manipulated_object_position(obj_position_actual)
+    cam_sim.set_manipulated_object_orientation_euler(obj_orientation_actual)
+    im_actual = cam_sim.render(cam_frame_R, cam_pos)
+
+    sam = SAMSegmentation()
+    mask_sam, score = sam.segment_image_center(im_actual, best_out_of_3=True)
+    plot_segmentation_mask(im_actual, mask_sam, mask_alpha=0.95, color=[30, 255, 30])
+
+    # TODO: plot masks intersections and IOU. implement the methods.
--- utils.py
+++ utils.py
@@ -9,3 +9,14 @@ def plot_segmentation_mask(im, mask, mask_alpha=0.8, color=np.array([255, 30, 30
     plt.imshow(segmented_im)
     plt.show()
 
+
+def plot_grid_segmentation_masks(images, masks, row_cols=(1,4), mask_alpha=0.8, color=np.array([255, 30, 30])):
+    fig, axs = plt.subplots(row_cols[0], row_cols[1], figsize=(20, 5))
+    axs = axs.reshape(-1)
+    for im, mask, ax in zip(images, masks, axs):
+        colored_mask = im.copy()
+        colored_mask[mask.squeeze() != 0] = color
+        segmented_im = (colored_mask/255) * mask_alpha + (im / 255) * (1 - mask_alpha)
+        ax.imshow(segmented_im)
+        ax.axis('off')
+    plt.show()
